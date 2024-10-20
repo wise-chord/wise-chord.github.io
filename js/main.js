6e9b9f94d66dd667f262fae7ce3e92fea21cb6bf
wise-chord <Wise_Chord@outlook.com>
2024-10-20 12:15:42 +0800

    add dark mode

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 // i18next init
 i18next.use(i18nextXHRBackend).init({
-    lng: 'en_GB', // 默认语言
+    // default lang
+    lng: getBrowserLanguagePreference(),
     backend: {
         loadPath: 'locales/{{lng}}.json'
     }
 }, function(err, t) {
-    // 初始化完成后，更新页面内容
+    // update the page after init
     updateContent();
+    setTheme(getBrowserTheme());
 });
 
+// get browser language preference
+function getBrowserLanguagePreference() {
+    const language = navigator.languages ? navigator.languages[0] : navigator.language;
+    return language || 'en_GB';
+}
+
+// update content language
 function updateContent() {
     document.querySelectorAll('[data-i18n]').forEach(function(element) {
         const key = element.getAttribute('data-i18n');
@@ -21,4 +30,15 @@ function setLanguage(language) {
         if (err) return console.error('Something went wrong loading', err);
         updateContent();
     });
+}
+
+function setTheme() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('.navbar').classList.toggle('dark-mode');
+    document.querySelectorAll('.dropdown-button').forEach(button => button.classList.toggle('dark-mode'));
+    document.querySelectorAll('.dropdown-content').forEach(content => content.classList.toggle('dark-mode'));
+    document.querySelectorAll('.navbar a').forEach(link => link.classList.toggle('dark-mode'));
+    document.querySelectorAll('button').forEach(button => button.classList.toggle('dark-mode'));
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
 }
